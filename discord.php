25cd18cbc54f0feed9c2d5d40e4c256e18270f8d
Nicholas Glantz <nike18054@gmail.com>
2017-07-03 15:50:10 -0700

    Add files via upload

---
+++ discord.php
@@ -0,0 +1,195 @@
+<html lang="en">
+<head>
+  <title>Clan B5TA</title>
+  <link rel="icon" href="https://i1.wp.com/b5ta.com/wp-content/uploads/2015/10/cropped-tnOKdrI.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Clan B5TA is an active social, skilling and bossing Runescape community that knows how to have fun. Founded: September 30, 2014">
+
+  <!--Dropdown hover css link-->
+  <link rel="stylesheet" type="text/css" href="css/dropdownhover.css">
+
+  <!--bootsrap css-->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+  <!-- JAVASCRIPT files-->
+  <script src="js/jquery.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/dropdown.js"></script>
+
+  <style>
+    body {
+      position: relative;
+    }
+    hr{
+      margin-left: 50px;
+      margin-right: 50px;
+    }
+    .head-format{
+      background-color:#ffffff;
+      color:#fff;
+      height:125px;
+    }
+    .logo{
+      height: 100%;
+      width: auto;
+      left: 5%;
+    }
+    .discfeature{
+      padding-top: 15px;
+      padding-bottom: 15px;
+      cursor: pointer;
+      border-radius: 25px;
+    }
+    .discfeature:hover{
+      background-color: #b3c4ff;
+    }
+    .discfeature-image{
+      margin: auto;
+      width: 75%;
+    }
+    /* Remove the navbar's default rounded borders and increase the bottom margin */
+    .navbar {
+      margin-bottom: 0px;
+      border-radius: 0;
+    }
+    /* Makes navbar opaque on hover*/
+    .navbar-inverse {
+      z-index: 1;
+      opacity: 0.90;
+      filter: alpha(opacity=90); /* For IE8 and earlier */
+    }
+    .navbar-inverse:hover {
+      opacity: 1.0;
+      filter: alpha(opacity=100); /* For IE8 and earlier */
+    }
+    /* Removes Carets on dropdown based on resize */
+    #caret{
+      display: inline-block;
+    }
+    @media (max-width: 767px) {
+      #caret{
+          display: none;
+      }
+      .dropdown:hover .dropdown-menu {
+          display: none;
+    }}
+    .affix {
+         top: 0;
+         width: 100%;
+     }
+     .affix + .container-fluid {
+         padding-top: 0px;
+         margin-top: 50px;
+     }
+     .container-fluid {
+        padding-right: 0px;
+        padding-left: 0px;
+
+      }
+   </style>
+
+  <body>
+
+    <div class="container-fluid head-format">
+      <div class="container">
+        <a href ="https://htmlpreview.github.io/?https://github.com/b5ta/b5ta-website/blob/master/index.php"><img class="logo" src="images/b5talogo.png" alt"b5talogo"></a>
+      </div>
+    </div>
+
+    <!--Left side of navbar-->
+    <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="125" >
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+
+            <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+                          <!--Home Navbar-->
+            <li><a href="https://htmlpreview.github.io/?https://github.com/b5ta/b5ta-website/blob/master/index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+
+            <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+                          <!--Discord Navbar-->
+            <li class="active"><a href="https://htmlpreview.github.io/?https://github.com/b5ta/b5ta-website/blob/master/discord.php"><img src="images/discord.jpg" class="img-circle" height="19px" width="19px"> Discord</a></li>
+
+
+            <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+                          <!--Guides dropdown-->
+            <li class="dropdown"><a class="dropdown-toggle" data-hover="dropdown" href="#"><span class="glyphicon glyphicon-list-alt"></span> Guides<span id="caret" class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="http://runescape.guide/bosses/" target="_blank">Bosses</a></li>
+                <li><a href="http://runescape.guide/gear/solo/" target="_blank">Gear</a></li>
+                <li><a href="http://runescape.guide/clue-scroll/" target="_blank">Invention Perks</a></li>
+                <li><a href="http://runescape.guide/free-runecoins/" target="_blank">Free Runecoins</a></li>
+                <li><a href="http://runescape.guide/clue-scroll/" target="_blank">Clue Scrolls</a></li>
+              </ul>
+            </li>
+
+            <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+                      <!-- Flip chart navbar-->
+            <li><a href="http://runescape.guide/flipping/"><span class="glyphicon glyphicon-usd"></span>Flipping</a></li>
+
+          </ul>
+
+
+
+          <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+                    <!--Login Navbar-->
+          <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Contact</a></li>
+            <li><a href"#"><span class="glyphicon glyphicon-user"></span> Log in</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+
+      <div class="row">
+        <div class="col-sm-3" style="padding-top: 15px;">
+          <img src="images/discord.jpg" alt="discord">
+        </div>
+
+        <div class="col-sm-9">
+          <h1 style="text-align:center;">Join our Discord server!</h1>
+          <br>
+          <br>
+          <h3>Download Discord Here: <a href="https://discordapp.com/download">https://discordapp.com/download</a></h3>
+          <br>
+          <h3>After you download the client, click this invite link to join our clan's Discord server: <a href="https://discord.gg/0qfZioFZLSnmWMs7">https://discord.gg/0qfZioFZLSnmWMs7</a></h3>
+        </div>
+      </div>
+      <br>
+      <hr>
+      <br>
+      <div class="row">
+        <h2 style="text-align:center;">What does our Discord server offer?</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-4 discfeature">
+          <h4 style="text-align:center;">Bossing</h4>
+          <img src="images/Beastmaster_Durzag.png" class="img-responsive discfeature-image" alt="Bossing" width="350" height="350">
+        </div>
+        <div class="col-sm-4 discfeature">
+          <h4 style="text-align:center;">RuneInfo</h4>
+          <img src="images/runeinfo.png" class="img-responsive discfeature-image" alt="RuneInfo" width="350" height="350">
+        </div>
+        <div class="col-sm-4 discfeature">
+          <h4 style="text-align:center;">Flipchat</h4>
+          <img src="images/coins.png" class="img-responsive discfeature-image" alt="Flipchat" width="350" height="350">
+        </div>
+      </div>
+
+    </div>
+
+</body>
